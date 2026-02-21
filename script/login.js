5f92c5bca53fe3973fb057a900768c7ec887e249
kazimizan77 <kazimizan77@yahoo.com>
2026-02-21 11:46:22 +0600

    update

--- script/login.js
+++ script/login.js
@@ -9,7 +9,7 @@ document.getElementById('login-btn').addEventListener('click', function () {
 
     if (contactNumber == "01234567890" && pin == "1234") {
         alert('login success')
-        window.location.assign('/home.html');
+        window.location.assign('home.html');
     } else {
         alert('login failed')
         return;
